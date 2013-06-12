0e7e3bc61e088f47e5a51ca07e7412926d0ae52c
Paweł Pawlik <pawel.pawlik@millwardbrown.com>
2013-06-12 12:42:00 +0200

    Gettext not working

--- ib-dotnet/Global.asax.cs
+++ ib-dotnet/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using i18n;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -21,6 +22,7 @@ namespace ib_dotnet
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            I18N.Register();
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
